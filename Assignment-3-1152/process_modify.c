0edc2811198aac424cf3d48495271bdcabba892b
Farah <farahnaz8124@gmail.com>
2025-10-08 23:36:31 +0500

    Added lab-3-hometask on oct-8

---
+++ Assignment-3-1152/process_modify.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid;
+
+    // Create a new process
+    pid = fork();
+
+     if (pid == 0) {
+        
+        execlp("date", "date", NULL);
+        
+        // If execlp returns, it failed
+        perror("execlp failed");
+    } else{
+       
+        wait(NULL); 
+        printf("Child process finished");
+    }
+    
+    return 0;
+}
